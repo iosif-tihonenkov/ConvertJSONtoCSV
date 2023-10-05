2dad424f8ca17676798f46ee114bc23a95c0e5b2
iosif-tihonenkov <iosif.tihonenkov@gmail.com>
2023-10-05 15:43:59 +0300

    Revert "Создал рабочую связку сервер-клиент с мин. функцией"
    This reverts commit d681ec495abcbd96dae8af65d6891f8bffe01226.

--- client.py
+++ client.py
@@ -1,19 +1,12 @@
 import socket
-import struct
-import os
 
-def send_file(sck: socket.socket, filename):
-    #Получение размера файла
-    filesize = os.path.getsize(filename)
-    #Сообщаем серверу, сколько байт будет отправлено
-    sck.sendall(struct.pack("<Q", filesize))
-    #Отправка файла блоками по 1024 байта
-    with open(filename, "rb") as f:
-        while read_bytes := f.read(1024):
-            sck.sendall(read_bytes)
-with socket.create_connection(('127.0.0.1', 3000)) as conn:
-    print("Подключение к серверу")
-    print("Передача файла...")
-    send_file(conn, "example.json")
-    print("Отправлено.")
-print("Соединение закрыто.")
+send_data = input("Введите число: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 3000))
+
+client.sendall(bytes(send_data, 'utf-8'))
+data = client.recv(1024).decode('utf-8')
+
+print("Квадрат вашего числа: ", data)
+
+client.close()
